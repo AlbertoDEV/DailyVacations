7e42f21172a0551b48c1724676af8edd34c4a884
Alberto González <118272715+AlbertoDEV@users.noreply.github.com>
2025-10-23 12:57:52 +0200

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,1 @@
 CREATE SCHEMA IF NOT EXISTS dailyvacations;
-
--- Eliminar la tabla de historial de Flyway de cualquier esquema donde pueda haber quedado.
-DROP TABLE IF EXISTS public.flyway_schema_history;
-DROP TABLE IF EXISTS dailyvacations.flyway_schema_history;
